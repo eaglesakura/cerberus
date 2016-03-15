e52ddc3a4ab2dd75ca59062b94fd8cb9a1b544fa
eaglesakura <eagle.sakura@gmail.com>
2016-03-15 10:32:57 +0900

    UnitTestの基底クラスを変更

--- src/test/java/com/eaglesakura/android/rx/RxTaskBuilderTest.java
+++ src/test/java/com/eaglesakura/android/rx/RxTaskBuilderTest.java
@@ -2,7 +2,7 @@ package com.eaglesakura.android.rx;
 
 import org.junit.Test;
 
-public class RxTaskBuilderTest extends CiJUnitTester {
+public class RxTaskBuilderTest extends UnitTestCase {
 
     @Test
     public void stub() {
