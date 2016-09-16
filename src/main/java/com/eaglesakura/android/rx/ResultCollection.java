8abcd3c459e02283e7d77e56bb5968248708db99
eaglesakura <eaglesakura@SQUALL.local>
2016-09-16 18:05:32 +0900

    型を指定しない簡易的なputを追加

--- src/main/java/com/eaglesakura/android/rx/ResultCollection.java
+++ src/main/java/com/eaglesakura/android/rx/ResultCollection.java
@@ -1,11 +1,12 @@
 package com.eaglesakura.android.rx;
 
+import android.support.annotation.NonNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 複数アイテムを返却するためのUtil
- *
  */
 public class ResultCollection {
     Map<Object, Object> mObjects = new HashMap<>();
@@ -20,10 +21,36 @@ public class ResultCollection {
         return this;
     }
 
+    /**
+     * valueの型(.class)をキーにして集合に追加する。
+     * 返却がすべて異なる型である場合のみ有効
+     */
+    public ResultCollection put(@NonNull Object value) {
+        return put(value.getClass(), value);
+    }
+
     public <T> T get(Object key) {
         synchronized (mObjects) {
             if (mObjects.containsKey(key)) {
                 return (T) mObjects.get(key);
+            }
+            throw new Error();
+        }
+    }
+
+    /**
+     * 自動で結果を検索して返す。
+     * キャストが成功した場合のみ返却する。
+     * これはputされている値がすべて違う型である場合のみ正しく動作する。
+     */
+    public <T> T get() {
+        synchronized (mObjects) {
+            for (Object value : mObjects.values()) {
+                try {
+                    return (T) value;
+                } catch (ClassCastException e) {
+
+                }
             }
             throw new Error();
         }
